14fd7cb734ec01fa90623937acbcb348ab72d26f
Ivan <ivan@ivan.168.1.159>
2025-01-23 13:54:50 -0600

    Remove env whatsapp number and added it into code

--- src/app/whatsapp-button/page.tsx
+++ src/app/whatsapp-button/page.tsx
@@ -2,10 +2,9 @@ import Link from "next/link";
 import { BsWhatsapp } from "react-icons/bs";
 
 export default function WhatsappButton() {
-    const whatsappNumber = process.env.WHATSAPP_NUMBER;
     return (
         <div className="w-full flex justify-center items-center">
-            <Link href={`https://wa.me/${whatsappNumber}`}
+            <Link href={`https://wa.me/522213374152`}
                 className="flex custom float w-[60px] h-[60px] mb-[40px] mr-[40px] bg-[#25d366] text-white rounded-full items-center justify-center z-50"
                 target="_blank"
                 rel="noopener noreferrer"
