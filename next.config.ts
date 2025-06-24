8645c1d5b6a0b812da215521158f6d2d5f4c8dca
edlingao <sonido244@gmail.com>
2025-06-24 00:06:05 -0600

    Testing open next implementation

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "standalone",
   images: {
     unoptimized: true,
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  // experimental: {
-  //   optimizeCss: true,
-  // },
   compress: true,
   poweredByHeader: false,
   generateEtags: false,
@@ -23,3 +20,6 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
